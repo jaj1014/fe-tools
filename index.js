36a792d3a7c50bc7d30a193061617007bc165023
jaj1014 <ajax@pendo.io>
2019-09-26 08:29:22 -0400

    Updtaed the vue tool with better logging and added usage comment

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+/*
+    USAGE:
+    Copy the below class and paste into the console to access the defined tools.
+*/
+
 const AJAX_FE_TOOLS = function initialize() {
-    const inspectVue = function getVC(){
-        return (function dudeWheresMyVue (element){
-            while(!element.__vue__){
-                element = element.parentNode;
+    const inspectVue = function getVC() {
+        return (function dudeWheresMyVue(element) {
+            try {
+                while (!element.__vue__) {
+                    element = element.parentNode;
+                }
+                return element.__vue__;
+            } catch (e) {
+                console.warn('Make sure you have selected a vue element');
             }
-           return element.__vue__;
         })($0)
     };
 
